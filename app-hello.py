c8af838290f2311677ed18dc9215d371897b2cfb
Burak Ergün <burak3ergun@gmail.com>
2020-05-25 23:09:21 +0300

    Docker File

--- app-hello.py
+++ app-hello.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "Welcome!"
-	
-app.route('/how are you')
+    return "Welcome!"
+
+@app.route('/how are you')
 def hello():
-	return 'I am good, how about you?'
-	
-if __name__="__main"
-	app.run(host"0.0.0.0", port=8080)
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
